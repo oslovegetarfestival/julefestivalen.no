eaf8dbb084a8f850f586b2809e3369a10aa416be
tommyno <tommy.nordum@gmail.com>
2023-10-05 18:29:12 +0200

    Add new footer graphics

--- web/components/Footer/Footer.tsx
+++ web/components/Footer/Footer.tsx
@@ -10,80 +10,81 @@ import { Button } from "components/Button"
 export const Footer = () => {
   return (
     <>
-      {/* Note: -1px is needed to avoid subpixel white line below */}
-      <div style={{ overflow: "hidden", marginBottom: "-1px" }}>
+      <div className={styles.illustrations}>
         <AnimationFooter />
       </div>
       <footer className={styles.footer}>
-        <Section centerContent>
-          <a href="#">Til toppen ↑</a>
-        </Section>
-        <Section width="full" verticalPadding="large">
-          <Flow>
-            <Grid>
-              <Flow space="small">
-                <h2 className="font-strike">Om julemarkedet</h2>
-                <p>
-                  Opplev det beste fra planteriket! Vi er en inspirerende og
-                  idyllisk matfestival som brenner for plantebasert mat.{" "}
-                  <Link href="/om-oss" className="link link--white">
-                    Mer om julemarkedet
+        <Section width="large">
+          <Section centerContent>
+            <a href="#">Til toppen ↑</a>
+          </Section>
+          <Section width="full" verticalPadding="large">
+            <Flow>
+              <Grid>
+                <Flow space="small">
+                  <h2 className="font-strike">Om julemarkedet</h2>
+                  <p>
+                    Opplev det beste fra planteriket! Vi er en inspirerende og
+                    idyllisk matfestival som brenner for plantebasert mat.{" "}
+                    <Link href="/om-oss" className="link link--white">
+                      Mer om julemarkedet
+                    </Link>
+                  </p>
+                  <Button
+                    color="white"
+                    link="https://tikkio.com/tickets/41042-oslo-vegetarfestivals-julemarked-2023"
+                    isOpenInNewWindow
+                  >
+                    Kjøp billett
+                  </Button>
+                </Flow>
+
+                <Flow space="small">
+                  <h2 className="font-strike">Kontakt</h2>
+                  <p>
+                    <strong>E-post</strong>
+                    <br />
+                    <a href="mailto:kontakt@oslovegetarfestival.no">
+                      kontakt@oslovegetarfestival.no
+                    </a>
+                  </p>
+                  <p>
+                    <strong>Facebook</strong> <br />
+                    <a href="https://www.facebook.com/OsloVegetarfestival">
+                      @oslovegetarfestival
+                    </a>
+                  </p>
+                  <p>
+                    <strong>Instagram</strong> <br />
+                    <a href="https://www.instagram.com/oslovegetarfestival/">
+                      @oslovegetarfestival
+                    </a>
+                  </p>
+                </Flow>
+
+                <Flow space="small">
+                  <h2 className="font-strike">Nyhetsbrev</h2>
+                  <p>
+                    Få siste nytt om plantebasert mat, konkurranser, utlodninger
+                    og mye mer!
+                  </p>
+                  <Newsletter />
+                </Flow>
+              </Grid>
+
+              <Flex justify="spaceBetween" align="end" wrap>
+                <p className="h1">
+                  <Link href="/" className={styles.logo}>
+                    <span>Oslo</span> <span>Vegetar</span>
+                    <span>festival</span>
                   </Link>
                 </p>
-                <Button
-                  color="white"
-                  link="https://tikkio.com/tickets/41042-oslo-vegetarfestivals-julemarked-2023"
-                  isOpenInNewWindow
-                >
-                  Kjøp billett
-                </Button>
-              </Flow>
-
-              <Flow space="small">
-                <h2 className="font-strike">Kontakt</h2>
-                <p>
-                  <strong>E-post</strong>
-                  <br />
-                  <a href="mailto:kontakt@oslovegetarfestival.no">
-                    kontakt@oslovegetarfestival.no
-                  </a>
-                </p>
-                <p>
-                  <strong>Facebook</strong> <br />
-                  <a href="https://www.facebook.com/OsloVegetarfestival">
-                    @oslovegetarfestival
-                  </a>
-                </p>
-                <p>
-                  <strong>Instagram</strong> <br />
-                  <a href="https://www.instagram.com/oslovegetarfestival/">
-                    @oslovegetarfestival
-                  </a>
-                </p>
-              </Flow>
-
-              <Flow space="small">
-                <h2 className="font-strike">Nyhetsbrev</h2>
-                <p>
-                  Få siste nytt om plantebasert mat, konkurranser, utlodninger
-                  og mye mer!
-                </p>
-                <Newsletter />
-              </Flow>
-            </Grid>
-
-            <Flex justify="spaceBetween" align="end" wrap>
-              <p className="h1">
-                <Link href="/" className={styles.logo}>
-                  <span>Oslo</span> <span>Vegetar</span>
-                  <span>festival</span>
+                <Link href="/personvern" className="link link--white">
+                  Personvern og cookies
                 </Link>
-              </p>
-              <Link href="/personvern" className="link link--white">
-                Personvern og cookies
-              </Link>
-            </Flex>
-          </Flow>
+              </Flex>
+            </Flow>
+          </Section>
         </Section>
       </footer>
     </>
