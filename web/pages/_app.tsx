d7de14fb3637ffbb4eb5f201e55d54807931d93d
tommyno <tommy.nordum@gmail.com>
2023-10-17 13:52:07 +0200

    Update Piwik tracking code

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -73,7 +73,7 @@ function MyApp({ Component, pageProps }: AppProps) {
             var qP=[];dataLayerName!=="dataLayer"&&qP.push("data_layer_name="+dataLayerName),isStgDebug&&qP.push("stg_debug");var qPString=qP.length>0?("?"+qP.join("&")):"";
             tags.async=!0,tags.src="https://oslovegetarfestival.containers.piwik.pro/"+id+".js"+qPString,scripts.parentNode.insertBefore(tags,scripts);
             !function(a,n,i){a[n]=a[n]||{};for(var c=0;c<i.length;c++)!function(i){a[n][i]=a[n][i]||{},a[n][i].api=a[n][i].api||function(){var a=[].slice.call(arguments,0);"string"==typeof a[0]&&window[dataLayerName].push({event:n+"."+i+":"+a[0],parameters:[].slice.call(arguments,1)})}}(i[c])}(window,"ppms",["tm","cm"]);
-            })(window, document, 'dataLayer', 'd5d3a6f1-f408-40b5-ba3b-d2a7f4bc7400');
+            })(window, document, 'dataLayer', 'd8416078-8712-4de4-b86d-1245c62b2127');
           `,
         }}
       />
